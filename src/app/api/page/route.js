df05c3971f96a01e5c8c7fb55529ba5e4c5a8fbe
Somesh saini <158258175+SomeshSaini-1@users.noreply.github.com>
2025-02-10 16:43:02 +0530

    Update route.js

--- src/app/api/page/route.js
+++ src/app/api/page/route.js
@@ -1,18 +1,36 @@
-
-
 import { NextResponse } from "next/server";
+
+export const dynamic = "force-static"; // Ensuring the page is statically generated
+export const revalidate = 10; // Revalidate the static page every 10 seconds
 
 export async function GET() {
   return NextResponse.json({ message: "Hello from App Router API!" });
 }
 
 export function PUT(req) {
- return NextResponse.json({ text: 'Hello' });
+  return NextResponse.json({ text: 'Hello' });
 }
 
-export async function POST(res) {
-  const data = await res.json();
+export async function POST(req) {
+  const data = await req.json();
   return NextResponse.json({
     data,
-  })
+  });
 }
+
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   return NextResponse.json({ message: "Hello from App Router API!" });
+// }
+
+// export function PUT(req) {
+//  return NextResponse.json({ text: 'Hello' });
+// }
+
+// export async function POST(res) {
+//   const data = await res.json();
+//   return NextResponse.json({
+//     data,
+//   })
+// }
